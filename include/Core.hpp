c868d0ae4fb22f7ca8d22777538bab936bbe30ae
Filip Dimovski <filip.dimovskifico@gmail.com>
2025-11-25 22:27:15 +0100

    VkValidationLayers

---
+++ include/Core.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+// External Libraries
+#include <vulkan/vulkan.h>
+#include <GLFW/glfw3.h>
+
+// Standard Libraries
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+
+// Constants
+const uint32_t WIDTH = 1600;
+const uint32_t HEIGHT = 900;
